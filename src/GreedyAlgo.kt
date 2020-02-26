3d29afaea54e88425b0166c33a2c83c9e49cb601
ckddlr0610 <aackddlr@gmail.com>
2020-02-26 18:27:20 +0900

    Greedy Ing

--- src/GreedyAlgo.kt
+++ src/GreedyAlgo.kt
@@ -1,3 +1,53 @@
+/**
+ * acmicpc.net/problem/5585
+ * Greedy Algorithm problem
+ */
 class GreedyAlgo {
+    private var coin = 0
+    private var count = IntArray(6)
+    private var coinArr = arrayOf(500, 100, 50, 10, 5, 1)
 
+    fun solution(price : Int): IntArray {
+        coin = 1000-price
+
+        while (!(coin==0)){
+            coin = coin%500
+            coin = coin%100
+            coin = coin%50
+        }
+
+        /*while (coin>=500){
+            coin = coin%500
+            count[0]++
+        }
+        while (coin>=100){
+            coin = coin%100
+            count[1]++
+        }
+        while (coin>=50){
+            coin = coin%50
+            count[2]++
+        }
+        while (coin>=10){
+            coin = coin%10
+            count[3]++
+        }
+        while (coin>=5){
+            coin = coin%5
+            count[4]++
+        }
+        while (coin>=1){
+            coin = coin%1
+            count[5]++
+        }*/
+        return count
+    }
+}
+
+fun main() {
+    var greedy = GreedyAlgo()
+    var solution : IntArray = greedy.solution(600)
+    for (i in solution){
+        println(i)
+    }
 }
